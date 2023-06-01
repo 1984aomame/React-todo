168f06c38abcf33d019749aed0e9d0f4bbc3a220
Tomoka Nohara <35630132+1984aomame@users.noreply.github.com>
2023-06-01 10:45:59 +0900

    テキストボックスより入力 入力内容の保持

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import "./styles.css";
 
 export const App = () => {
   const { useEffect, useState } = React;
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["aaaa", "iiiii"]);
   const [completeTodos, setCompleteTodos] = useState(["uuuu"]);
-
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    const newtodos = [...incompleteTodos, todoText];
+    return alert("Add");
+  };
   return (
     <>
       <div className="input-area">
-        <input id="add-text" placeholder="TODOを入力" />
-        <button id="add-button">追加</button>
+        <input
+          id="add-text"
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onChange={onClickAdd} id="add-button">
+          追加
+        </button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
@@ -19,7 +31,7 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={onClickAdd}>完了</button>
                 <button>削除</button>
               </div>
             );
