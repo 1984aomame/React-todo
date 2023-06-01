43ac43036d10ca889fb7680fb16343d053bfeb43
Tomoka Nohara <35630132+1984aomame@users.noreply.github.com>
2023-06-01 16:37:25 +0900

    CSS in JS の例 現場ではCSSはファイルわけすると思うので参考程度

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "300px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props; //分割代入
   return (
-    <div className="input-area">
+    <div style={style}>
       <input
         id="add-text"
         placeholder="TODOを入力"
