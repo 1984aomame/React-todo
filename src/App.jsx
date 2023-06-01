53167dbf062bc3fe7c48ca4db7d48b9e39d36b55
Tomoka Nohara <35630132+1984aomame@users.noreply.github.com>
2023-06-01 10:10:11 +0900

    html構築。完了未完了の初期値をひとます投入

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,43 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  const { useEffect, useState } = React;
+  const [incompleteTodos, setIncompleteTodos] = useState(["aaaa", "iiiii"]);
+  const [completeTodos, setCompleteTodos] = useState(["uuuu"]);
+
+  return (
+    <>
+      <div className="input-area">
+        <input id="add-text" placeholder="TODOを入力" />
+        <button id="add-button">追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul id="incomplete-list">
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
+        <ul id="complete-list">
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
